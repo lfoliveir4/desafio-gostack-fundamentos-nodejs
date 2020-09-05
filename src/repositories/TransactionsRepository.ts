e6c9204cb73850c59da65779ea413427121cb25d
Luis Filipe Alves de Oliveira <macbook-pro-lf-oliveira@MacBook-Pro-de-Luis.local>
2020-09-05 11:37:33 -0300

    Fixed Task

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
-import Transaction from '../models/Transaction';
+import Transaction from "../models/Transaction";
+
+interface CreateTransactionDTO {
+  title: string;
+  type: "income" | "outcome";
+  value: number;
+}
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+enum Type {
+  INCOME = "income",
+  OUTCOME = "outcome",
 }
 
 class TransactionsRepository {
@@ -14,15 +25,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private calculateBalance(operator: Type): number {
+    return this.transactions
+      .filter((transaction) => transaction.type === operator)
+      .reduce((acc, tr) => acc + tr.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance(Type.INCOME);
+    const outcome = this.calculateBalance(Type.OUTCOME);
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+
+    if (type === Type.OUTCOME && total < value) {
+      throw Error("Cannot create outcome transaction without a valid balance");
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
